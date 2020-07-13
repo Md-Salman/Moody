1aa23fdbcee9e736f076b70f5018d66ac0558e67
Md-Salman <ms7017033150@gmail.com>
2020-07-13 04:33:58 -0700

    Layout made in expo managed workflow

--- App.js
+++ App.js
@@ -1,19 +1,127 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
+import data from './Data';
+import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+  const [isLoaded] = useFonts({ 
+    'TitleFont': require('./assets/fonts/Roboto-Medium.ttf'),
+  })
+
+  if(!isLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+      <View style={styles.container}>
+        <FlatList 
+          data={data}
+          renderItem={ ({ item }) => (
+            // Card
+            <View style={styles.card}>
+  
+              {/* Title */}
+              <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <View>
+                  <Text style={styles.onlienText}>Online</Text>
+                  <Text style={styles.onlineNumText}>{item.online}</Text>
+                </View>
+              </View>
+  
+              {/* Image  */}
+              <View style={styles.imageContainer}>
+                  <Image source={item.image} style={styles.image}/>
+              </View>
+              <View>
+                <View style={{flexDirection: 'row'}}>
+                  <TouchableOpacity style={styles.icon}>
+                    <MaterialIcons name="chat-bubble" size={32} color="#ebebeb" />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.icon}>
+                    <Ionicons name="ios-call" size={32} color="#ebebeb" />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.icon}>
+                    <Ionicons name="ios-videocam" size={32} color="#ebebeb" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+      </>
+    );
+  }
 }
 
+
+const backgroundColor = '#282828'
+const IconColor = '#ebebeb'
+const textYellow = '#ffcc00'
+
 const styles = StyleSheet.create({
+  statusBar: {
+    height: Constants.statusBarHeight,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'black',
+    paddingHorizontal: 3,
   },
+  card: {
+    width: '100%',
+    marginBottom: 3,
+    borderRadius: 10,
+    backgroundColor: backgroundColor,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 32,
+    fontFamily: 'TitleFont',
+    letterSpacing: 3,
+  },
+  onlienText: {
+    color: '#00cb3d',
+    fontFamily: 'TitleFont',
+  },
+  onlineNumText: {
+    fontFamily: 'TitleFont',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    paddingVertical: 5,
+    height: 150,
+  },
+  image: {
+    resizeMode: 'contain',
+    height: '100%',
+  },
+  eyeIcon: {
+    textAlign: 'center',
+  },
+  onlineNumText: {
+    flex: 2/3,
+    textAlignVertical: 'center',
+    color: '#ebebeb',
+  },
+  icon: {
+    flex: 1,
+    paddingVertical: 15,
+    alignItems: 'center'
+  }
 });
---
+++ Data.js
@@ -0,0 +1,38 @@
+export default data = [
+    {
+        key: "1",
+        title: 'Sing',
+        image: require('./assets/emojis/Sing.png'),
+        online: 234567
+    },
+    {
+        key: "2",
+        title: 'Dance',
+        image: require('./assets/emojis/Dance.png'),
+        online: 234567
+    },
+    {
+        key: '3',
+        title: 'Love',
+        image: require('./assets/emojis/Love.png'),
+        online: 234567
+    },
+    {
+        key: '4',
+        title: 'Bad',
+        image: require('./assets/emojis/Bad.png'),
+        online: 234567
+    },
+    {
+        key: '5',
+        title: 'Fun',
+        image: require('./assets/emojis/Fun.png'),
+        online: 234567
+    },
+    {
+        key: '6',
+        title: 'Sad',
+        image: require('./assets/emojis/Sad.png'),
+        online: 234567
+    },
+]
